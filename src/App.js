d3982bd14db119d916dd66c7621660fd3b978c08
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2024-06-02 20:00:47 -0300

    create footer and add useContext

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import { useContext } from "react";
 import "./App.css";
 import CharacterGRid from "./Components/CharacterGrid";
 import Header from "./Components/Header";
 import MainCharacter from "./Components/MainCharacter";
+import DBContext from "./Context/GlobalContext";
+import { getCharactersData } from "./Utils/FetchApi";
+import { useState } from "react";
 
 function App() {
+    const { gallery, setGallery } = useContext(DBContext);
+    const [characters, setCharacters] = useState([]);
+
+    getCharactersData("./Data/db.json");
+
     return (
         <div className='App'>
             <Header />
             <MainCharacter />
             <CharacterGRid />
+            <footer className='Footer'>
+                <button
+                    onClick={() => {
+                        setGallery(!gallery);
+                    }}
+                    className='Main__button'>
+                    {gallery ? "ver menos" : "ver mas"}
+                </button>
+            </footer>
         </div>
     );
 }
