dacca0787e67f026db4570d7695d0e97dc1dbc03
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2024-06-11 14:12:59 -0300

    added main character info

--- src/Components/MainCharacter.jsx
+++ src/Components/MainCharacter.jsx
@@ -6,21 +6,42 @@ import { useContext } from "react";
 import DBContext from "../Context/GlobalContext";
 
 function Main_character() {
-    const { gallery } = useContext(DBContext);
+    const { gallery, characters, mainCharacter } = useContext(DBContext);
+    console.log(mainCharacter, "log from the MAIN");
     return (
         <main className={`pt-4 pb-4 ${gallery ? "hide" : ""}`}>
             <figure className='flex justify-center flex-col w-11/12 mx-auto'>
-                <img className='Main__image' src={Goku} alt='Goku' />
-                <h3 className='text-yellow-400 uppercase font-bold text-2xl'>
-                    SAIYAJIN
-                </h3>
+                <img
+                    className='Main__image'
+                    src={
+                        mainCharacter.length <= 0
+                            ? Goku
+                            : mainCharacter[0].imageSrc
+                    }
+                    alt='Goku'
+                />
+                <figcaption className='flex items-center justify-start'>
+                    <h3 className='text-yellow-400 uppercase font-bold text-2xl'>
+                        {mainCharacter.length <= 0
+                            ? characters[0].raze
+                            : mainCharacter[0].raze}
+                    </h3>
+                    <hr className='w-2 mx-2' />
+                    <span className='text-yellow-600 uppercase font-bold text-lg'>
+                        {mainCharacter.length <= 0
+                            ? `Universe ${characters[0].universe}`
+                            : `Universe ${mainCharacter[0].universe}`}
+                    </span>
+                </figcaption>
                 <h2 className='text-white uppercase font-bold text-5xl'>
-                    GOKU
+                    {mainCharacter.length <= 0
+                        ? characters[0].name
+                        : mainCharacter[0].name}
                 </h2>
                 <p className='text-white font-medium pt-2'>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Cum totam dicta consectetur aperiam, fugiat excepturi
-                    laudantium nisi culpa soluta quaerat.
+                    {mainCharacter.length <= 0
+                        ? characters[0].desc
+                        : mainCharacter[0].desc}
                 </p>
             </figure>
         </main>
