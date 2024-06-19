b0692366f5b884b232c1d33ab954958acdd4693f
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2024-06-19 10:24:22 -0300

    add img responsive sizes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,17 @@
 import React from "react";
-import Logo from "../Assets/dragonballLogo.png";
+import Logo from "../Assets/dragonballLogo.webp";
 import "./Header.css";
 
 function Header() {
     return (
         <header className='w-full items-center flex justify-between px-2'>
-            {/* <section>
-                <i>
-                    <span className='material-symbols-outlined'>sort</span>
-                </i>
-            </section> */}
             <figure className='w-full flex justify-center'>
                 <img
                     className='Header__logo'
                     src={Logo}
                     alt='Dragon Ball Logo'
+                    sizes='(max-width: 600px) 300px, (max-width: 1200px) 600px, 1200px'
+                    loading='lazy'
                 />
             </figure>
         </header>
